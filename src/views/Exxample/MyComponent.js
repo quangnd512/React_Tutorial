7165df03dd0223e406a94b3a5649917049ae8fa9
quangnd512 <quangnd512@gmail.com>
2022-02-16 23:43:53 +0700

    React - Components

---
+++ src/views/Exxample/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return ( 
+            <div> Hello </div>
+        )
+    }
+}
+
+// Sử dụng để export default để trả về 1 class MyComponent nằm trong file MyComponent
+export default MyComponent;
