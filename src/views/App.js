89a5bb19f7d335540debd32a9b04e28aafb50807
quangnd512 <quangnd512@gmail.com>
2022-02-17 06:45:03 +0700

    React - DOM vs setState

--- src/views/App.js
+++ src/views/App.js
@@ -11,14 +11,6 @@ const App = () => { // Arrow function
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
         <MyComponent /> {/*Component*/}
       </header>
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -7,11 +7,23 @@ class MyComponent extends React.Component {
         name: 'Quang ND',
         age: 27
     }
+
+    // setState
+    onChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    onChangeAge = (event) => {
+        this.setState({
+            age: event.target.value
+        })
+    }
     
     render() {
         // JSX
         let name = 'AHT';
-
 
         return ( 
             //Sử dụng thẻ ẩn bao quanh 2 thẻ chính
@@ -19,6 +31,15 @@ class MyComponent extends React.Component {
                 {/* call jsx */}
                 <div className="first"> Hello {name} </div>
                 <div className="secord"> {name} JSC </div>
+
+                <div className="info-input">
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.onChangeName(event)}
+                    />
+                    <input value={this.state.age} type="number" 
+                        onChange={(event) => this.onChangeAge(event)}
+                    />
+                </div>
 
                 {/* call state */}
                 <div className="title">Name: {this.state.name}, Age: {this.state.age}</div>
